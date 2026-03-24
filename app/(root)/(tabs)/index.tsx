d899545e88000c891589ca6343ba08d59b057640
Altamash-Khan-2812 <altamashkhn281@gmail.com>
2026-03-24 20:27:12 +0530

    add simple header with hard-coded values

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,6 +1,29 @@
-import { Text } from "react-native";
+import icons from "@/constants/icons";
+import images from "@/constants/images";
+import { Image, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import "../../../globals.css";
 
 export default function Index() {
-  return <Text>Hello world</Text>;
+  return (
+    <SafeAreaView className="bg-white h-full">
+      <View className="px-5">
+        <View className="flex flex-row items-center justify-between mt-5">
+          <View className="flex flex-row items-center">
+            <Image source={images.avatar} className="size-12 rounded-full" />
+            <View className="flex flex-col items-start ml-2 justify-center">
+              <Text className="text-xs font-rubik text-black-100">
+                Good Morning
+              </Text>
+              <Text className="text-base font-rubik-medium text-black-300">
+                John Doe
+              </Text>
+            </View>
+          </View>
+
+          <Image source={icons.bell} className="size-6" />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
 }
