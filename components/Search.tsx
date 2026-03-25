26c6a8b6558c8e44c36e23b8a1515246f2e2ad0d
Altamash-Khan-2812 <altamashkhn281@gmail.com>
2026-03-25 12:02:35 +0530

    add search component

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,7 +1,37 @@
-import React from "react";
+import icons from "@/constants/icons";
+import { useLocalSearchParams, usePathname } from "expo-router";
+import React, { useState } from "react";
+import { Image, TextInput, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Search = () => {
-  return <div>Search</div>;
+  const path = usePathname();
+  const params = useLocalSearchParams<{ query?: string }>();
+  const [search, setSearch] = useState(params.query || "");
+
+  function handleSearch(text: string) {
+    setSearch(text);
+  }
+
+  return (
+    <SafeAreaView>
+      <View className="flex flex-row items-center justify-between w-full px-4 rounded-lg bg-accent-100 border border-primary-100 mt-5 py-2">
+        <View className="flex-1 flex-row items-center justify-center z-50 py-2">
+          <Image source={icons.search} className="size-5" />
+          <TextInput
+            placeholder="Search for anything"
+            value={search}
+            onChangeText={handleSearch}
+            className="flex-1 text-sm font-rubik text-black-300 py-0 mt-2 ml-2"
+          />
+        </View>
+
+        <TouchableOpacity>
+          <Image source={icons.filter} className="size-5" />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
 };
 
 export default Search;
